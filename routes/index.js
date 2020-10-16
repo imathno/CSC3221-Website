e1f403718652c81ea4736a5267ad751c1658bab6
Issa Mathno <imathno@gmail.com>
2020-10-15 18:37:55 -0700

    Fix image thing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('../public/html/main.html');
+  res.sendFile(path.join(__dirname + '../public/html/main.html'));
 });
 
 module.exports = router;
